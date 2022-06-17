6696f649915fbe9835b6f75d7e9db12c3575a80f
DKProtocol <damann1072@gmail.com>
2022-06-17 09:49:58 -0500

    Updates

---
+++ precache-manifest.28e09278b004a58f58a0a9ba9975f457.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7427225aa53450e6255aac90016e51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d882b0442623248d4a6a",
+    "url": "/static/css/2.7cc9718e.chunk.css"
+  },
+  {
+    "revision": "0f9c033bbeb3accda82b",
+    "url": "/static/css/main.0242b2e8.chunk.css"
+  },
+  {
+    "revision": "d882b0442623248d4a6a",
+    "url": "/static/js/2.fe7c1620.chunk.js"
+  },
+  {
+    "revision": "f6ce9b198908e234c073cb4697347be2",
+    "url": "/static/js/2.fe7c1620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f9c033bbeb3accda82b",
+    "url": "/static/js/main.29757080.chunk.js"
+  },
+  {
+    "revision": "c5bb459f079dc2a36d65",
+    "url": "/static/js/runtime-main.cf9698b9.js"
+  },
+  {
+    "revision": "5394859b3490c3f2d2d59623f7bea910",
+    "url": "/static/media/Logo.5394859b.png"
+  },
+  {
+    "revision": "f36d91f0e061c40e0656d3d4fd72dc43",
+    "url": "/static/media/game_of_squids-webfont.f36d91f0.woff"
+  }
+]);
